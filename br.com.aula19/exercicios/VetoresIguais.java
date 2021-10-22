82547f40250d974adefe4c65eeadd36f54a97557
Letícia dos Santos <lds.leticia.dos.santos@gmail.com>
2021-10-22 22:11:56 +0000

    primeiro exercício

--- br.com.aula19/exercicios/VetoresIguais.java
+++ br.com.aula19/exercicios/VetoresIguais.java
@@ -1,11 +1,18 @@
 public class VetoresIguais{
 
     public static void main(String[] args) {
-        int[] vetorA;
-        int[] vetorB;
-
-        vetorA = [1,2,3,4,5];
-        vetorB = vetorA;
-
-        System.out.printls(vetorA[] + " " + vetorB[]);
+       
+    int[] vetorA = new int [5];
+    vetorA[0] = 1;
+    vetorA[1] = 2;
+    vetorA[2] = 3;
+    vetorA[3] = 4;
+    vetorA[4] = 5;
+    
+    int[] vetorB = vetorA.clone(); 
+    
+    
+    for(double itens : vetorA){
+            System.out.println(itens);
+        }
 }
